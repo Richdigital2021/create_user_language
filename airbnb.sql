fb62dfbc1c8cdc8c6c64f4d929f011aff75c576b
mutheeEverlyn <123722613+mutheeEverlyn@users.noreply.github.com>
2025-04-06 18:15:29 +0300

    created category table (#28)
    * created category table
    
    * category table
    
    ---------
    
    Co-authored-by: PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>

--- airbnb.sql
+++ airbnb.sql
@@ -1,8 +1,8 @@
--- create database and tables for airbnb project
+-- create airbnb database
 CREATE DATABASE airbnb;
 
-
-
-
-
+CREATE TABLE category(
+id INT PRIMARY KEY,
+category_name varchar(50) NOT NULL
+);
 
