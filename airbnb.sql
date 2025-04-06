b18b02dd280a8293d6f94a90aea0caea888609c3
Shadrack <shadrackmeshack7@gmail.com>
2025-04-07 01:27:51 +0300

    Create attribute_category table with id and category_name fields

--- airbnb.sql
+++ airbnb.sql
@@ -6,3 +6,8 @@ id INT PRIMARY KEY,
 category_name varchar(50) NOT NULL
 );
 
+-- Create attribute_category table
+CREATE TABLE attribute_category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
