9b0e0fba0edc2f29397e086d89ba84ab7241c2ef
Richdigital2021 <akintunderichard28@gmail.com>
2025-04-07 21:16:14 +0100

    solution to #23 'create user_languae table'

--- airbnb.sql
+++ airbnb.sql
@@ -11,3 +11,14 @@ CREATE TABLE attribute_category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(255) NOT NULL
 );
+
+-- create user_language table
+CREATE TABLE user_language (
+    user_id INT NOT NULL,
+    language_id INT NOT NULL,
+    PRIMARY KEY (user_id, language_id),
+    FOREIGN KEY (user_id) REFERENCES user_account(id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (language_id) REFERENCES language(id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
